d3e710fffc2be34b424f61dc7615adb3d75da586
Alejandrogg7 <61033432+Alejandrogg7@users.noreply.github.com>
2020-05-09 20:34:20 -0500

    Progreso fire base

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,3 +3,18 @@ import '../styles/index.scss';
 import 'bootstrap';
 
 console.log('webpack starterkit');
+
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAFtsGI4iG7i3YkDRjp0QHvKfMDqUSlyjQ",
+    authDomain: "todolist2-71587.firebaseapp.com",
+    databaseURL: "https://todolist2-71587.firebaseio.com",
+    projectId: "todolist2-71587",
+    storageBucket: "todolist2-71587.appspot.com",
+    messagingSenderId: "754207503880",
+    appId: "1:754207503880:web:c520906ec66794ff0f43bd"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
